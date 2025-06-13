122f7e97b0ed40ad7f9ed38063f0972c04297bf7
saditJ <sjaupaj08@gmail.com>
2025-06-13 13:51:49 +0200

    Clean up: removed Next.js files for static HTML/CSS version

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-black text-white">
-      <h1 className="text-3xl font-bold">Tribe Tirana coming soon...</h1>
-    </main>
-  )
-}
